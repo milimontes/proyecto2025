b4eabbe54c09bd65a69be85d8fb7b58c8f6c9a9a
araguilani <166127854+araguilani@users.noreply.github.com>
2024-11-15 12:37:25 -0300

    cambie la foto delas card card.component.ts

--- src/app/modules/inicio/components/card/card.component.ts
+++ src/app/modules/inicio/components/card/card.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 // IMPORTAMOS INTERFAZ
 import { Productos } from 'src/app/models/tarjetas';
-
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
@@ -10,12 +9,11 @@ import { Productos } from 'src/app/models/tarjetas';
 export class CardComponent {
   // PROPIEDAD PÚBLICA (TIPO ARRAY)
   public info: Productos[];
-
   constructor() {
     this.info = [
       {
         id: "",
-        imagen: "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/5c01b6060b9647e8811daf630117bcaf_9366/Zapatilla_Campus_00s_Gris_HQ6507_01_standard.jpg",
+        imagen: "https://static.zara.net/assets/public/3443/656e/ff2143c19102/3e90eb6c98e8/12341120800-e1/12341120800-e1.jpg?ts=1724407576624&w=563",
         nombre: "𝒛𝒂𝒑𝒂𝒕𝒊𝒍𝒍𝒂𝒔",
         alt: "Zapatillas",
         liked: false // propiedad para el "me gusta"
@@ -23,25 +21,23 @@ export class CardComponent {
       {
         id: "",
         nombre: "𝒄𝒂𝒓𝒕𝒆𝒓𝒂",
-        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn0eTdLOegoOtpL3tBV9s5HCLDmK0WlfJUmA5Ea-juGTdWdFksdMQNCsrtsO7aIGHq0TQ&usqp=CAU",
+        imagen: "https://static.zara.net/assets/public/bbd1/9db9/6bf14cddb842/421a47dc7ee3/13600420800-e1/13600420800-e1.jpg?ts=1722425340717&w=563",
         alt: "Cartera",
         liked: false
       },
       {
         id: "",
         nombre: "𝒈𝒐𝒓𝒓𝒂",
-        imagen: "https://newera.com.ar/media/catalog/product/cache/06cfaa02c67cf3a5c3c05d775284c631/g/o/gorra-new-era-new-york-yankees-59fifty-citrus-pop-60288266_7_.jpg",
+        imagen: "https://static.zara.net/assets/public/a10e/9e9b/949741bca1cc/bdfa0f1db678/05875408400-e1/05875408400-e1.jpg?ts=1711009227920&w=563",
         alt: "Gorra",
         liked: false
       }
     ]
   }
-
   // Método para manejar el evento Like
   likeProducto(i: any) {
     i.liked = !i.liked; // Cambia el estado del "like"
   }
-
   // Método para manejar el evento Share
   shareProducto(i: any) {
     console.log('Compartir: ', i.nombre);
