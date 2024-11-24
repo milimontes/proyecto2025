4f0a394a11fa05cb42958eb386d329f732d19dec
araguilani <166127854+araguilani@users.noreply.github.com>
2024-11-24 17:57:55 +0000

    admin

--- src/app/modules/admin/services/crud.service.ts
+++ src/app/modules/admin/services/crud.service.ts
@@ -2,123 +2,133 @@ import { Injectable } from '@angular/core';
 import { Producto } from 'src/app/models/producto';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { map } from 'rxjs';
+// Importaciones para el manejo de archivos y referencias de Storage
 import { getDownloadURL, getStorage, ref, UploadResult, uploadString, deleteObject } from 'firebase/storage';
 
 /*
-  getDownloadURL -> Para obtener la URL de descarga de una imagen subida.
-  getStorage -> Para obtener la instancia de almacenamiento en Firebase.
-  ref -> Para crear referencias a ubicaciones en el almacenamiento.
-  UploadResult -> Tipo que representa el resultado de una operación subida.
-  uploadString -> Para subir imágenes en formato de cadena (string).
-  deleteObject -> Para eliminar un objeto en el almacenamiento (como una imagen).
+  getDownloadURL -> Para obtener la URL de descarga de una imagen subida
+  getStorage -> Para obtener la instancia de almacenamiento
+  ref -> Para crear referencias a ubicaciones en el almacenamiento
+  UploadResult -> Tipo que representa el resultado de una operación subida
+  uploadString -> Para subir imágenes en formato de cadena (string)
+  deleteObject -> Para eliminar un espacio en el almacenamiento
 */
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
-  // Definimos la colección de productos de la base de datos de Firestore
-  private productosCollection: AngularFirestoreCollection<Producto>;
+  // Definimos colección para los productos de la web del tipo Producto
+  private productosCollection: AngularFirestoreCollection<Producto>
 
-  // Variable para almacenar la respuesta de la operación de subida de imagen
+  // Definimos variable "respuesta" que podrá subir resultados
   private respuesta!: UploadResult;
 
-  // Inicializamos la instancia de Firebase Storage
+  // Inicializamos servicio de Storage
   private storage = getStorage();
 
   constructor(private database: AngularFirestore) {
-    // Referenciamos la colección de productos en la base de datos de Firestore
+    // Referenciamos colección productos y será subida como "producto" a Firebase
     this.productosCollection = database.collection('producto');
   }
 
-  // Método para CREAR productos
+  // CREAR productos -> obtiene datos del formulario y url de la imagen
   crearProducto(producto: Producto, url: string){
     return new Promise(async (resolve, reject) => {
-      try {
-        // Creamos un nuevo ID para el producto en la base de datos
+      try{
+        // Creamos número identificativo para el producto en la base de datos
         const idProducto = this.database.createId();
 
-        // Asignamos el ID generado al atributo idProducto de la interfaz Producto
+        // Asignamos ID creado al atributo idProducto de la interfaz "Producto"
         producto.idProducto = idProducto;
 
-        // Asignamos la URL de la imagen al atributo imagen del producto
+        // Asignamos URL recibida del parámetro al atributo imagen de la interfaz Producto
         producto.imagen = url;
 
-        // Guardamos el producto en Firestore utilizando el ID generado
         const resultado = await this.productosCollection.doc(idProducto).set(producto);
 
         resolve(resultado);
-      } catch (error) {
+      }catch(error){
         reject(error);
       }
-    });
+    })
   }
 
-  // Método para OBTENER productos de la base de datos
+  // OBTENER productos
   obtenerProducto(){
-    // snapshotChanges toma una "instantánea" de los datos y map convierte los datos a formato adecuado
-    return this.productosCollection.snapshotChanges().pipe(
-      map(action => action.map(a => a.payload.doc.data()))
-    );
+    // snapshotChanges -> toma una captura del estado de los datos
+    // pipe -> funciona como una tubería que retorna el nuevo arreglo de datos
+    // map -> "mapea" o recorre esa nueva información
+    // a -> resguarda la nueva información y la envía
+    return this.productosCollection.snapshotChanges().pipe(map(action => action.map(a => a.payload.doc.data())));
   }
 
-  // Método para EDITAR un producto
+  // EDITAR productos
   modificarProducto(idProducto: string, nuevaData: Producto){
-    // Actualizamos los datos del producto en Firestore utilizando el ID del producto
+    // accedemos a la colección, buscamos por ID y actualizamos información
     return this.database.collection('producto').doc(idProducto).update(nuevaData);
   }
 
-  // Método para ELIMINAR un producto de la base de datos y su imagen de Firebase Storage
+  // ELIMINAR productos
   eliminarProducto(idProducto: string, imagenUrl: string){
     return new Promise((resolve, reject) => {
-      try {
-        // Referencia al archivo de imagen en Firebase Storage usando su URL
-        const referenciaImagen = ref(this.storage, imagenUrl);
+      try{
+        // Definimos referencia local de Storage en el bloque "try"
+        const storage = getStorage();
 
-        // Eliminamos la imagen de Firebase Storage
+        // Definimos referencia local desde el almacenamiento de Storage
+        const referenciaImagen = ref(storage, imagenUrl);
+
+        // Eliminamos la imagen desde el almacenamiento
         deleteObject(referenciaImagen)
         .then((res) => {
-          // Eliminamos el producto de la base de datos Firestore
+          // Accedo a la colección, busco su ID y lo elimino
           const respuesta = this.productosCollection.doc(idProducto).delete();
           resolve(respuesta);
         })
         .catch(error => {
-          reject("Error al eliminar la imagen: \n" + error);
-        });
-      } catch (error) {
+          reject("Error al eliminar la imagen: \n"+error);
+        })
+      }
+      catch(error){
         reject(error);
       }
-    });
+    })
   }
 
-  // Método para OBTENER la URL de descarga de una imagen
+  // OBTENER url de las imágenes
   obtenerUrlImagen(respuesta: UploadResult){
-    // Retorna la URL obtenida de la imagen subida
+    // Retorna URL obtnenida como REFERENCIA
     return getDownloadURL(respuesta.ref);
   }
 
   /**
-   * Método para SUBIR imágenes con sus referencias a Firebase Storage
-   * @param {string} nombre - Nombre del archivo de imagen
-   * @param {any} imagen - Datos de la imagen (en formato base64 o similar)
-   * @param {string} ruta - Ruta dentro del almacenamiento en Firebase
-   * @returns - Retorna la respuesta con la información sobre la subida de la imagen
-   */
+   * PARÁMETROS DEFINIDOS
+   * @param {string} nombre <- nombre de la imagen
+   * @param {any} imagen <- tipo de imágenes que se pueden subir (extension)
+   * @param {string} ruta <- ruta de almacenamiento de las imágenes
+   * @returns <- se retorna lo obtenido
+  */
+
+  // SUBIR imágenes con sus referencias
   async subirImagen(nombre: string, imagen: any, ruta: string){
-    try {
-      // Creamos una referencia para el archivo de imagen en Firebase Storage
+    try{
+      // Crear una referencia de imagen
+      // accede a Storage (almacenamiento), ruta (carpeta) / nombre (nombreImagen)
       let referenciaImagen = ref(this.storage, ruta + '/' + nombre);
 
-      // Subimos la imagen como una cadena base64 utilizando 'uploadString'
+      // Asignamos a la respuesta la información de la imagen subida
       this.respuesta = await uploadString(referenciaImagen, imagen, 'data_url')
       .then(resp => {
-        return resp; // Retornamos el resultado de la subida
-      });
+        return resp;
+      })
 
       return this.respuesta;
-    } catch (error) {
-      console.log("Error al subir imagen: \n" + error);
-      return this.respuesta; // Retornamos la respuesta, aunque haya ocurrido un error
+    }
+    catch(error){
+      console.log("Error: \n"+error);
+
+      return this.respuesta;
     }
   }
 }
